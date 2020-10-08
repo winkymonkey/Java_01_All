b8b2de1c2749f20f80482e03302e9441fd206d80
apal5 <apal5@intuit.com>
2020-10-09 01:26:14 +0530

    test

--- src/main/java/com/example/java/i_immutable/ImmutableClass.java
+++ src/main/java/com/example/java/i_immutable/ImmutableClass.java
@@ -1,44 +1,57 @@
 package com.example.java.i_immutable;
 
-
 import java.util.Date;
+import java.util.HashMap;
+
 
 /**
  * Always remember that your instance variables will be either mutable or immutable.
- * Identify them and return new objects with copied content for all mutable objects.
+ * Identify the mutable objects and return new objects with content copied in it.
  * Immutable variables can be returned safely without extra effort.
  */
+@SuppressWarnings("unchecked")
 public final class ImmutableClass {
 
-	private final Integer immutableField1;		//Integer class is immutable (does not provide setter to change its content)
-	private final String immutableField2;		//String class is immutable (does not provide setter to change its content)
-	private final Date mutableField;			//Date class is mutable (provides setter to change various date/time parts)
+	private final Integer immutableField1;			//Integer class is immutable (doesn't provide setter to change its content)
+	private final String immutableField2;			//String class is immutable (doesn't provide setter to change its content)
+	private final Date mutableField;				//Date class is mutable (provides setter to change various date/time parts)
+	private final HashMap<String,String> mutableMap;//HashMap class is mutable (provides setter to change its content)
 	
 	
-	//Private constructor ensures no unplanned construction of class
-	ImmutableClass(Integer fld1, String fld2, Date date) {
+	//Constructor performing Deep Copy
+	public ImmutableClass(Integer fld1, String fld2, Date date, HashMap<String,String> map) {
 		this.immutableField1 = fld1;
 		this.immutableField2 = fld2;
 		this.mutableField = new Date(date.getTime());
+		
+		HashMap<String,String> newMap = new HashMap<String,String>();
+		map.forEach((key,val)->newMap.put(key, val));
+		this.mutableMap = newMap;
 	}
 
 	//NO SETTER METHOD
 	
-	//Integer class is immutable so we can return the instance variable as it is
+	//Integer class is immutable. So we can safely return the instance variable
 	public Integer getImmutableField1() {
 		return immutableField1;
 	}
-	//String class is immutable so we can return the instance variable as it is
+	//String class is immutable. So we can safely return the instance variable
 	public String getImmutableField2() {
 		return immutableField2;
 	}
 
-	/**
-	 * Date class is mutable so we need a little care here. We should not return the reference of original instance variable.
-	 * Instead a new Date object, with content copied to it, should be returned.
-	 */
+	
+	//Date class is mutable so we should not return the reference of instance variable
+	//Instead a cloned copy of the field should be returned
 	public Date getMutableField() {
 		return new Date(mutableField.getTime());
 	}
+	
+	//HashMap class is mutable so we should not return the reference of instance variable
+	//Instead a cloned copy of the field should be returned
+	public HashMap<String, String> getMutableMap() {
+		return (HashMap<String, String>)mutableMap.clone();
+	}
+	
 }
 
