2c537d6e1931842f94b9eedf9134da72c48e149f
apal5 <apal5@intuit.com>
2020-10-09 19:44:31 +0530

    test

--- src/main/java/com/example/java/l_collection/G_LinkedHashMap_AccessOrder.java
+++ src/main/java/com/example/java/l_collection/G_LinkedHashMap_AccessOrder.java
@@ -4,19 +4,18 @@ import java.util.LinkedHashMap;
 import java.util.Map;
 
 
-public class LinkedHashMap_AccessOrder {
+public class G_LinkedHashMap_AccessOrder {
 	public static void main(String[] args) {
-		
 		Map<String, Integer> map = new LinkedHashMap<String, Integer>(16, 0.75F, true);
 		map.put("A", 1);
 		map.put("B", 2);
 		map.put("C", 3);
-		map.entrySet().forEach(entry -> System.out.println(entry.getKey() + "--" + entry.getValue()));
+		
+		map.entrySet().forEach(entry -> System.out.println(entry.getKey()));
 		
 		System.out.println(map.get("B"));
 		
-		map.entrySet().forEach(entry -> System.out.println(entry.getKey() + "--" + entry.getValue()));
+		map.entrySet().forEach(entry -> System.out.println(entry.getKey()));
 	}
+	
 }
-
-
